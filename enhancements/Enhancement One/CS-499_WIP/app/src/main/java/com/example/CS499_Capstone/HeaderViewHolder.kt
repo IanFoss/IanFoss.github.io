47d1183addaefd3a9ae270031b879a4d137c8c10
Ian Foss <131810639+IanFoss@users.noreply.github.com>
2026-06-05 10:15:24 -0400

    Delete enhancements/Enhancement One/CS-499_WIP directory

--- enhancements/Enhancement One/CS-499_WIP/app/src/main/java/com/example/CS499_Capstone/HeaderViewHolder.kt
+++
@@ -1,7 +0,0 @@
-package com.example.CS499_Capstone
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-// Viewholder for header row of recyclerview
-class HeaderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
